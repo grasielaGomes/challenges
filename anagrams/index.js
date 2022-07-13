d49a906e99b0cfda8a5df7b70535b5ef8b4a20c5
Grasiela Gomes <produtoradama@gmail.com>
2022-07-13 09:43:24 -0300

    feat: add O(n) anagram function

--- anagrams/index.js
+++ anagrams/index.js
@@ -12,8 +12,27 @@ function anagrams(stringA, stringB) {
   return cleanString(stringA) === cleanString(stringB);
 }
 
+// Anagram with sorting O(n log n) time and space complexity
 function cleanString(string) {
   return string.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
+
+// Anagram with objects O(n) time and space complexity
+function anagrams2(stringA, stringB) {
+  let a = {};
+  let b = {};
+  for (let char of stringA) {
+    a[char] = (a[char] || 0) + 1;
+  }
+  for (let char of stringB) {
+    b[char] = (b[char] || 0) + 1;
+  }
+  for (let key in a) {
+    if (!b[key] || a[key] !== b[key]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = anagrams;
